a0975aaf2d399683eab0489814369ec6810243df
majiandong <836240219@qq.com>
2017-01-26 23:57:33 +0800

    Update

--- Introduction to Algorithms/Chapter2/BinarySearch.java
+++ Introduction to Algorithms/Chapter2/BinarySearch.java
@@ -3,15 +3,11 @@ public class BinarySearch{
 
 	public static void main(String[] args) {
 		int[] arr = new int[]{12,23,28,34,46,61,87,99};
-		int index = binarySearch(arr,0,arr.length - 1, 11);
+		int index = binarySearch(arr,0,arr.length - 1, 28);
 		System.out.println(index);
 	}
 
 	public static int binarySearch(int[] arr, int first, int last, int value){
-
-		// find mid value mid，
-		// if value < mid，then find mid value between first to mid; vice versa.
-		// repeat until find the value
 
 		while(first <= last){
 			int mid = (first + last)/2;
