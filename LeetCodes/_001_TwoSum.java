db67da85e09836afd8dc21fe856910c8bd41e787
majiandong <836240219@qq.com>
2017-07-22 13:14:23 +0800

    another method for twoSum

--- LeetCodes/_001_TwoSum.java
+++ LeetCodes/_001_TwoSum.java
@@ -1,5 +1,5 @@
 import java.util.HashMap;
-
+import java.util.Arrays;
 /**
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 You may assume that each input would have exactly one solution.
@@ -13,8 +13,8 @@ Example
 public class _001_TwoSum {
 
 	public static void main(String[] args) {
-		int[] nums = {2, 7, 11, 15};
-		int[] twoSum = twoSum(nums, 9);
+		int[] nums = {150,24,79,50,88,345,3};
+		int[] twoSum = twoSum2(nums, 200);
 		System.out.println("["+twoSum[0]+","+twoSum[1]+"]");
 	}
 
@@ -31,5 +31,22 @@ public class _001_TwoSum {
 			}
 		}
 		return result;
+	}
+
+	public static int[] twoSum2(int[] nums, int target) {
+		int len = nums.length;
+		for (int i = 0; i < len; i++) {
+			int end = len - 1;
+			while(i < end){
+				int delta = nums[i] + nums[end] - target;
+				if(delta != 0){
+					end--;
+				}else{
+					System.out.println(i + "" + end);
+					return new int[]{i,end};
+				}
+			}
+		}
+		return new int[]{0,0};
 	}
 }
