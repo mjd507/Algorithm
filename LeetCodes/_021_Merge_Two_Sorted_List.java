d9b2fdd1a14f2a1c8dc85801cdb7ecdd95f8e8d8
majiandong <836240219@qq.com>
2017-07-26 21:42:47 +0800

    递归实现

--- LeetCodes/_021_Merge_Two_Sorted_List.java
+++ LeetCodes/_021_Merge_Two_Sorted_List.java
@@ -9,14 +9,14 @@ public class _021_Merge_Two_Sorted_List{
     ListNode ans = new ListNode(0);
     ListNode temp = ans;
     while(l1 != null && l2 != null){
-      temp = temp.next;
       if(l1.val < l2.val){
-        temp = l1;
+        temp.next = l1;
         l1 = l1.next;
       }else {
-        temp = l2;
+        temp.next = l2;
         l2 = l2.next;
       }
+      temp = temp.next;
     }
     if(l1 == null){
       temp.next = l2;
@@ -25,5 +25,19 @@ public class _021_Merge_Two_Sorted_List{
       temp.next = l1;
     }
     return ans.next;
+  }
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    if(l1 == null) return l2;
+    if(l2 == null) return l1;
+    
+    ListNode node;
+    if(l1.val < l2.val){
+      node = l1;
+      node.next = mergeTwoLists(l1.next, l2);
+    }else {
+      node = l2;
+      node.next = mergeTwoLists(l1, l2.next);
+    }
+    return node;
   }
 }
