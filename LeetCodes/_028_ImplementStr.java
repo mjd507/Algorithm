51b68f5553670fef68d5e7e4eff93ae70354c9df
majiandong <836240219@qq.com>
2017-09-24 23:35:59 +0800

    feat: ImplementStr
    https://leetcode.com/problems/implement-strstr/description/

---
+++ LeetCodes/_028_ImplementStr.java
@@ -0,0 +1,19 @@
+
+public class _028_ImplementStr{
+
+  public static void main(String[] args) {
+    int index = strStr("","");
+    System.out.println(index);
+  }
+  public static int strStr(String haystack, String needle) {
+    if(needle.length() == 0) return 0;
+    int stackLen = haystack.length();
+    int needLen = needle.length();
+    for (int i = 0; i < stackLen - needLen; i++) {
+      for (int j = 0; j < needLen && needle.charAt(j) == haystack.charAt(i+j); j++) {
+        if(j == needLen - 1) return i;
+      }
+    }
+    return -1;
+  }
+}
