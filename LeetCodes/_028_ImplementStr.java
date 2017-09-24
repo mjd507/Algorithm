420408daffc35659d35809a55ab1b59409c7cdd0
majiandong <836240219@qq.com>
2017-09-24 23:45:46 +0800

    update

--- LeetCodes/_028_ImplementStr.java
+++ LeetCodes/_028_ImplementStr.java
@@ -9,7 +9,7 @@ public class _028_ImplementStr{
     if(needle.length() == 0) return 0;
     int stackLen = haystack.length();
     int needLen = needle.length();
-    for (int i = 0; i < stackLen - needLen; i++) {
+    for (int i = 0; i <= stackLen - needLen; i++) {
       for (int j = 0; j < needLen && needle.charAt(j) == haystack.charAt(i+j); j++) {
         if(j == needLen - 1) return i;
       }
